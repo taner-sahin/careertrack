86d8d21b35b8662333fab78e3064332f48e3d747
Taner Sahin <kemal.liya19@gmail.com>
2026-07-22 23:51:06 +0300

    Complete Application model and admin configuration

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from companies.models import Company
 
-# Create your models here.
+class Application(models.Model):
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    position = models.CharField(max_length=150)
+    status = models.CharField(max_length=20,default='Applied')
+    application_date = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True)
+    
+    def __str__(self):
+     return f"{self.company} - {self.position}"
--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
